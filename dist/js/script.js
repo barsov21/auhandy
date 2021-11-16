6b89717a84f718b72920535ebba4b71dc037e49e
barsov21 <89139175+barsov21@users.noreply.github.com>
2021-11-16 15:16:48 +0300

    Add files via upload

---
+++ dist/js/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+    // Modal 
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+    
+ 
+    function valideForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите своё имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                    required: "Пожалуйста, введите свой почтовый адрес",
+                    email: "Неправильно введён адрес почты"
+                }
+            }
+        });
+    }
+
+    valideForms('#consultation-form');
+    valideForms('#consultation form');
+
+    $('input[name=phone]').mask("+7 (999) 999-9999");
+    
+});
